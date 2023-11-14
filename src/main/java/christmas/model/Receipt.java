5b19a04b55e10a31640f7bad7f35344373f80c6f
noah <jeongas9810@gmail.com>
2023-11-15 00:18:24 +0900

    feat: Add validate count method in Receipt object

--- src/main/java/christmas/model/Receipt.java
+++ src/main/java/christmas/model/Receipt.java
@@ -1,12 +1,36 @@
 package christmas.model;
 
+import common.MenuItems;
+
 public class Receipt {
+    private final int MENU_MIN_COUNT = 1;
     private final Menu menu;
     private final int count;
 
     public Receipt(Menu menu, int count) {
+        validateCount(count);
         this.menu = menu;
         this.count = count;
+    }
+
+    public Receipt(String koName, int count) {
+        validateCount(count);
+        this.menu = MenuItems.getMenuByName(koName);
+        this.count = count;
+    }
+
+    private void validateCount(int count) {
+        if (count < MENU_MIN_COUNT) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public Integer getCount() {
+        return count;
     }
 
     @Override
