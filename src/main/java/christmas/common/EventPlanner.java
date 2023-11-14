15c21a8f99fa376c136cc2ec62008e41f2c3b0a8
noah <jeongas9810@gmail.com>
2023-11-15 02:58:02 +0900

    feat: reservation() return Order class

--- src/main/java/christmas/common/EventPlanner.java
+++ src/main/java/christmas/common/EventPlanner.java
@@ -1,24 +1,32 @@
 package christmas.common;
 
+import christmas.model.Order;
 import christmas.model.Receipt;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class EventPlanner implements Planner{
-    private OutputView outputView = new OutputView();
-    private InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+
+    private final static Integer YEAR = 2023;
+    private final static Integer MONTH = 12;
 
     @Override
     public void start() {
-        reservation();
+        Order order = reservation();
     }
 
-    public void reservation() {
+    @Override
+    public Order reservation() {
         outputView.printWelcomeMsg();
         int estimatedDay = inputView.readDate();
+        LocalDate date = LocalDate.of(YEAR, MONTH, estimatedDay);
         List<Receipt> receipts = inputView.readOrderMenu();
         outputView.printIntro(estimatedDay);
+        return new Order(date, receipts);
     }
 }
