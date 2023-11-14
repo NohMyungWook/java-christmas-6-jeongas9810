62f6bb3e6b03e57a51b5800b187250d7b5f264d9
noah <jeongas9810@gmail.com>
2023-11-15 03:36:45 +0900

    feat: Add method checkCanParticipate in interface

--- src/main/java/christmas/common/Planner.java
+++ src/main/java/christmas/common/Planner.java
@@ -1,8 +1,12 @@
 package christmas.common;
 
+import christmas.model.Benefit;
 import christmas.model.Order;
+
+import java.util.List;
 
 public interface Planner {
     void start();
     Order reservation();
+    List<Benefit> checkCanParticipate(Order order);
 }
