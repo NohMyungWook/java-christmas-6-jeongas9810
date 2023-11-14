47e2ef7edc50cc3338433e1662f9d22d92ca03b1
noah <jeongas9810@gmail.com>
2023-11-15 07:50:21 +0900

    test: use parameterizedTest
    Use this annotation to test for many inputs.

--- src/test/java/christmas/DayInputTest.java
+++ src/test/java/christmas/DayInputTest.java
@@ -3,6 +3,8 @@ package christmas;
 import christmas.view.InputValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -10,22 +12,25 @@ public class DayInputTest {
     private final InputValidator validator = new InputValidator();
 
     @DisplayName("문자열 입력 시 예외 발생")
-    @Test
-    void inputDayIsNotNumber() {
-        assertThatThrownBy(() -> Integer.parseInt("test"))
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "가나다라", "test", "3a"})
+    void inputDayIsNotNumber(String s) {
+        assertThatThrownBy(() -> Integer.parseInt(s))
                 .isInstanceOf(NumberFormatException.class);
     }
 
     @DisplayName("31보다 큰 숫자 입력 시 예외 발생")
-    @Test
-    void inputDayUpperThanThirtyOne() {
-        assertThatThrownBy(() -> validator.validateDay(32))
+    @ParameterizedTest
+    @ValueSource(ints = {32, 45, 68, 90, Integer.MAX_VALUE})
+    void inputDayUpperThanThirtyOne(int number) {
+        assertThatThrownBy(() -> validator.validateDay(number))
                 .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("1보다 작은 숫자 입력 시 예외 발생")
-    @Test
-    void inputDayLowerThanOne() {
-        assertThatThrownBy(() -> validator.validateDay(0))
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -5, -100, Integer.MIN_VALUE})
+    void inputDayLowerThanOne(int number) {
+        assertThatThrownBy(() -> validator.validateDay(number))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
