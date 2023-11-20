3eed78c16dcf5652d2fa8ad88cdd36410085b65c
noah <jeongas9810@gmail.com>
2023-11-21 05:05:23 +0900

    refactor: Loop always until user input valid value

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,45 +5,39 @@ import christmas.model.Receipt;
 import christmas.view.constant.ErrorMessage;
 import christmas.view.constant.ViewMessage;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
-    private final static int INIT_INT = 0;
     private final static String COMMA = ",";
     private final InputValidator validator = new InputValidator();
 
     public int readDate() {
         ViewMessage.ASK_DATE.print();
-        int day = INIT_INT;
 
-        while (day == INIT_INT) {
+        while (true) {
             try {
-                day = Integer.parseInt(Console.readLine().trim());
+                int day = Integer.parseInt(Console.readLine().trim());
                 validator.validateDay(day);
+                return day;
             } catch (IllegalArgumentException e) {
                 ErrorMessage.INPUT_DATE.print();
-                day = INIT_INT;
             }
         }
-        return day;
     }
 
     public List<Receipt> readOrderMenu() {
-        boolean isValidated = false;
-        List<Receipt> receipts = new ArrayList<>();
+        List<Receipt> receipts;
 
         ViewMessage.ASK_ORDER.print();
-        while (!isValidated) {
+        while (true) {
             try {
                 String[] menuOrder = Console.readLine().trim().split(COMMA);
                 receipts = validator.validateMenuOrder(menuOrder);
                 validator.validateReceipt(receipts);
-                isValidated = true;
+                return receipts;
             } catch (IllegalArgumentException e) {
                 ErrorMessage.INPUT_MENU.print();
             }
         }
-        return receipts;
     }
 }
